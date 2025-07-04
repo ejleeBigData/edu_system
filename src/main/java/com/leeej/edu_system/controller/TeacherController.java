9041aed401272aa76d3eeab9ba6d49eb8a69b8bd
이은정 <ej2grateful@gmail.com>
2025-07-04 15:12:37 +0900

    삭제 버튼까지

---
+++ src/main/java/com/leeej/edu_system/controller/TeacherController.java
@@ -0,0 +1,52 @@
+package com.leeej.edu_system.controller;
+
+import com.leeej.edu_system.model.Teacher;
+import com.leeej.edu_system.repository.TeacherRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.AttributedString;
+
+@Controller
+@RequestMapping("/teachers")
+@RequiredArgsConstructor
+public class TeacherController {
+    private final TeacherRepository teacherRepository;
+
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("teachers", teacherRepository.findAll());
+
+        return "teacher-list";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model){
+        model.addAttribute("teacher", new Teacher());
+
+        return "teacher-form";
+    }
+
+    @PostMapping("/add")
+    public String add(@ModelAttribute Teacher teacher) {
+        teacherRepository.save(teacher);
+
+        return "redirect:/teachers";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editForm(@PathVariable int id, Model model) {
+        model.addAttribute("teacher", teacherRepository.findById(id));
+
+        return "teacher-form";
+    }
+
+    @PostMapping("/edit")
+    public String edit(@ModelAttribute Teacher teacher) {
+        teacherRepository.update(teacher);
+
+        return "redirect:/teachers";
+    }
+}
---
+++ src/main/java/com/leeej/edu_system/model/Teacher.java
@@ -0,0 +1,15 @@
+package com.leeej.edu_system.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Teacher {
+    private Integer id;
+    private String name;
+}
